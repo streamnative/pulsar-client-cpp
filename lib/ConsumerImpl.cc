3f0f79737a621eff1232c5f5124cd8d7703cd09b
Yunze Xu <xyzinfernity@163.com>
2023-10-20 13:00:35 +0800

    Fix topic name is shown as a pointer rather than string (#331)
    ### Motivation
    
    This is an additional fix to #329 because I still observed logs like:
    
    ```
    Closing consumer for topic 0x6000028e0648
    Closing producer for topic 0x600001210b88
    ```
    
    It's because `HandlerBase::topic_` field is protected and could be
    accessed directly from the derived classes.
    
    ### Motivation
    
    In `HandlerBase`, make `topic_` private and add two methods `topic()`
    and `getTopicPtr()` to get the reference to the string and the shared
    pointer. `getTopicPtr()` should only be called when being passed to
    `MessageImpl::setTopicName`.
    
    (cherry picked from commit 7cefe0e66439da69a75168842ba867d41ea5f346)

--- lib/ConsumerImpl.cc
+++ lib/ConsumerImpl.cc
@@ -174,7 +174,7 @@ Future<Result, ConsumerImplBaseWeakPtr> ConsumerImpl::getConsumerCreatedFuture()
 
 const std::string& ConsumerImpl::getSubscriptionName() const { return originalSubscriptionName_; }
 
-const std::string& ConsumerImpl::getTopic() const { return *topic_; }
+const std::string& ConsumerImpl::getTopic() const { return topic(); }
 
 void ConsumerImpl::start() {
     HandlerBase::start();
@@ -194,7 +194,7 @@ void ConsumerImpl::start() {
 
     // Initialize ackGroupingTrackerPtr_ here because the get_shared_this_ptr() was not initialized until the
     // constructor completed.
-    if (TopicName::get(*topic_)->isPersistent()) {
+    if (TopicName::get(topic())->isPersistent()) {
         if (config_.getAckGroupingTimeMs() > 0) {
             ackGroupingTrackerPtr_.reset(new AckGroupingTrackerEnabled(
                 connectionSupplier, requestIdSupplier, consumerId_, config_.isAckReceiptEnabled(),
@@ -250,7 +250,7 @@ Future<Result, bool> ConsumerImpl::connectionOpened(const ClientConnectionPtr& c
     uint64_t requestId = client->newRequestId();
     LOG_WARN(getName() << "start subscribing the topic");
     SharedBuffer cmd = Commands::newSubscribe(
-        *topic_, subscription_, consumerId_, requestId, getSubType(), consumerName_, subscriptionMode_,
+        topic(), subscription_, consumerId_, requestId, getSubType(), consumerName_, subscriptionMode_,
         subscribeMessageId, readCompacted_, config_.getProperties(), config_.getSubscriptionProperties(),
         config_.getSchema(), getInitialPosition(), config_.isReplicateSubscriptionStateEnabled(),
         config_.getKeySharedPolicy(), config_.getPriorityLevel());
@@ -557,7 +557,7 @@ void ConsumerImpl::messageReceived(const ClientConnectionPtr& cnx, const proto::
 
     Message m(messageId, brokerEntryMetadata, metadata, payload);
     m.impl_->cnx_ = cnx.get();
-    m.impl_->setTopicName(topic_);
+    m.impl_->setTopicName(getTopicPtr());
     m.impl_->setRedeliveryCount(msg.redelivery_count());
 
     if (metadata.has_schema_version()) {
@@ -1245,7 +1245,7 @@ void ConsumerImpl::closeAsync(ResultCallback originalCallback) {
         return;
     }
 
-    LOG_INFO(getName() << "Closing consumer for topic " << topic_);
+    LOG_INFO(getName() << "Closing consumer for topic " << topic());
     state_ = Closing;
     incomingMessages_.close();
 
@@ -1766,7 +1766,7 @@ void ConsumerImpl::processPossibleToDLQ(const MessageId& messageId, ProcessDLQCa
                             return;
                         }
                         if (result != ResultOk) {
-                            LOG_WARN("{" << self->topic_ << "} {" << self->subscription_ << "} {"
+                            LOG_WARN("{" << self->topic() << "} {" << self->subscription_ << "} {"
                                          << self->consumerName_ << "} Failed to acknowledge the message {"
                                          << originMessageId
                                          << "} of the original topic but send to the DLQ successfully : "
@@ -1779,7 +1779,7 @@ void ConsumerImpl::processPossibleToDLQ(const MessageId& messageId, ProcessDLQCa
                         }
                     });
                 } else {
-                    LOG_WARN("{" << self->topic_ << "} {" << self->subscription_ << "} {"
+                    LOG_WARN("{" << self->topic() << "} {" << self->subscription_ << "} {"
                                  << self->consumerName_ << "} Failed to send DLQ message to {"
                                  << self->deadLetterPolicy_.getDeadLetterTopic() << "} for message id "
                                  << "{" << originMessageId << "} : " << res);
